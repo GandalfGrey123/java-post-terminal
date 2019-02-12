709f0c54d1df7d4d926ae98607dd1caa03f040d3
pabloegpf1 <pabloescrivagallardo@gmail.com>
2019-02-12 12:35:41 -0800

    Added main method and argument error control

--- Manager.java
+++ Manager.java
@@ -3,5 +3,12 @@ import services.*;
 import system.*;
 
 public class Manager{
+
+	public static void main(String[] args){
+		if(args.length != 1){
+			System.out.println("Error: Manager <REST API URL>");
+			return;
+		}
+	}
 	
 }
