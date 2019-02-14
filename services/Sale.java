eef4249bc758a61df85a406e7ae977aaf742c57f
Matt M <matt@puroxy.org>
2019-02-13 17:02:49 -0800

    SalesSerivce working with proper GET & PUT requests to correct endpoints. I also added bin/ folder for .class files to keep project code clean

--- services/Sale.java
+++ services/Sale.java
@@ -1,5 +1,36 @@
 package services;
 
-public class Sale{
-	
+import java.net.MalformedURLException;
+import java.io.IOException;
+
+public class Sale {
+
+    String baseUrl = "http://localhost:3000/sales";
+
+    Sale() {
+    }
+
+    String getSales() {
+        try {
+            Get req = new Get(baseUrl);
+            String res = req.execute();
+            return res;
+        } catch (MalformedURLException e) {
+            return e.getMessage();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    String newSale(String payload) {
+        try {
+            Put req = new Put(baseUrl);
+            String res = req.execute(payload);
+            return res;
+        } catch (MalformedURLException e) {
+            return e.getMessage();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
 }
